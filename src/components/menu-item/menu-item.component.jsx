1b43b7c9b7620e80ae08621527b3da40c6560480
Terence Ng Han How <ngtere@live.com.sg>
2020-06-25 02:11:00 +1000

    refactoring of project by components & pages

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${size} menu-item`}>
+  <div className={`${size} menu-item`}>
+
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className='background-image'>
+    </div>
+
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
