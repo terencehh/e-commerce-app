54e101997ec4967f2cd0132fb27c812b79631847
Terence Ng Han How <ngtere@live.com.sg>
2020-06-25 22:23:46 +1000

    header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 function App() {
 
   //
   return (
     <div>
+      {/* Ensure header is always present & rendered */}
+      <Header />
       {/* Switch ensures that if multiple path matches, only renders first path */}
       <Switch>
         <Route exact path='/' component={HomePage} />
