dad7ca305b7bdc9b401ee997646409d1c449a0fc
Terence Ng Han How <ngtere@live.com.sg>
2020-06-25 21:49:34 +1000

    setting up components for shopping page, shop items, and preview

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+// we need to store data of our shop collection hence requires state
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+
+  render() {
+    const { collections } = this.state;
+    return <div className='shop-page'>
+      {
+        collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+      }
+    </div>
+  }
+}
+
+export default ShopPage;
