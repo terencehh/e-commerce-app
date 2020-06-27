5dcad70b5386e86e0427bbd73c60d53db83c2451
Terence Ng Han How <ngtere@live.com.sg>
2020-06-27 18:25:07 +1000

    created add to cart button styling in shops page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-botton.component'
 
 import './collection-item.styles.scss'
 
@@ -13,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 )
 
--- src/components/custom-button/custom-botton.component.jsx
+++ src/components/custom-button/custom-botton.component.jsx
@@ -4,8 +4,8 @@ import './custom-button.styles.scss';
 // pull children of props
 // destructure other props into the button
 // e.g. if submit is passed into the button, it will receive it
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={`${inverted ? 'inverted' : ''}  ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
     {children}
   </button >
 )
