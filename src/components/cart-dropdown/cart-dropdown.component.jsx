a3e50d2bb097b3b38a78045b8ab79348f87ec3f9
Terence Ng Han How <ngtere@live.com.sg>
2020-06-28 00:01:05 +1000

    checkout box disappears after clicking checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import './cart-dropdown.styles.scss'
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {
@@ -18,7 +19,11 @@ const CartDropdown = ({ cartItems, history }) => (
           (<span className='empty-message'>Your cart is empty</span>)
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => {
+      history.push('/checkout')
+      dispatch(toggleCartHidden())
+    }
+    }>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
@@ -27,5 +32,6 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 })
 
+// react connect automatically gives us dispatch prop if we do not supply mapDispatchToProps
 // requires access to browser history/match API via prop
 export default withRouter(connect(mapStateToProps)(CartDropdown));
