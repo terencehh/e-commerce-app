610089080ebe5cc42b48b6802392ed4409f7badd
Terence Ng Han How <ngtere@live.com.sg>
2020-07-12 17:52:05 +0800

    refactored category component to prepare for future advanced routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
 // we need to store data of our shop collection hence requires state
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
-    {
-      collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))
-    }
+    <CollectionsOverview />
   </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
