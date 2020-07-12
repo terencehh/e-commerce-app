06a74a7337f302eeee39082201e328c877e481d1
Terence Ng Han How <ngtere@live.com.sg>
2020-07-12 20:02:52 +0800

    created layout to display individual sections of the shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,9 +6,15 @@ import { selectCollection } from '../../redux/shop/shop.selectors'
 
 const CollectionPage = ({ collection }) => {
   console.log(collection)
+  const { title, items } = collection;
   return (
-    <div className='category'>
-      <h2>Collection Page</h2>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
     </div>
   )
 }
