2f5e2a2fe511fa5cdc1e0086614bd1572313177e
Terence Ng Han How <ngtere@live.com.sg>
2020-07-12 19:08:50 +0800

    added memoization to selectCollection so that if function gets called again with same param, dont rerun and just return same value as previously

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,3 +1,5 @@
+import memoize from 'lodash.memoize';
+
 // create a memoized selector for our directory component
 // this ensures that components which depend on our state do not rerender whenever state changes that is unrelated to the user state.
 
@@ -16,4 +18,5 @@ const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
 
-export const selectCollection = collectionUrlParam => createSelector([selectCollections], collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]));
+// as collectionUrlParam is a dynamic argument we need to memoize the whole function using a memoize helper function
+export const selectCollection = memoize(collectionUrlParam => createSelector([selectCollections], collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])));
