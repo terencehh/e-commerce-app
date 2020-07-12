d1e272e5e4f26d0880dec4f864d07179a0b74731
Terence Ng Han How <ngtere@live.com.sg>
2020-07-12 20:15:29 +0800

    improved efficiency during navigation by performing data normalisation

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -5,18 +5,12 @@ import memoize from 'lodash.memoize';
 
 import { createSelector } from 'reselect';
 
-// object which matches a string value to its respective ID number
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  jackets: 2,
-  sneakers: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
 
 // as collectionUrlParam is a dynamic argument we need to memoize the whole function using a memoize helper function
-export const selectCollection = memoize(collectionUrlParam => createSelector([selectCollections], collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])));
+export const selectCollection = memoize(collectionUrlParam => createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
+));
